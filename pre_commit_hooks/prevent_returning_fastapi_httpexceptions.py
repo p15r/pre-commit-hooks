1ff90209745edcf9c92cc7846258a528287c5281
Patrick <patrick@p15r.net>
2025-03-15 16:36:23 +0000

    feat: detect returned FastAPI HTTPExceptions

--- pre_commit_hooks/prevent_returning_fastapi_httpexceptions.py
+++ pre_commit_hooks/prevent_returning_fastapi_httpexceptions.py
@@ -1,13 +1,43 @@
 from __future__ import annotations
 
 import argparse
-import re
+import ast
 import sys
-from pathlib import Path
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from collections.abc import Sequence
+
+
+class HTTPExceptionVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.http_exception_returns = []
+
+    def visit_FunctionDef(self, node):
+        self._process_function(node)
+
+    def visit_AsyncFunctionDef(self, node):
+        self._process_function(node)
+
+    def _process_function(self, node):
+        for stmt in ast.walk(node):
+            if isinstance(stmt, ast.Return) and isinstance(
+                stmt.value, ast.Call
+            ):
+                if getattr(stmt.value.func, 'id', None) in [
+                    'AFKException',
+                    'HTTPException',
+                    'WebSocketException',
+                ]:
+                    self.http_exception_returns.append(stmt.lineno)
+
+
+def find_http_exception_returns(file_path: str) -> list[str]:
+    with open(file_path, encoding='utf-8') as f:
+        tree = ast.parse(f.read(), filename=file_path)
+    visitor = HTTPExceptionVisitor()
+    visitor.visit(tree)
+    return visitor.http_exception_returns
 
 
 def main(argv: Sequence[str] | None = None) -> int:
@@ -16,16 +46,15 @@ def main(argv: Sequence[str] | None = None) -> int:
     args = parser.parse_args(argv)
 
     ret_val = 0
-
-    for filename in args.filenames:
-        print(f'Checking {filename}...')
-
-        with Path.open(filename) as f_handle:
-            rego = f_handle.readlines()
-
-        for line_no, line in enumerate(rego, start=1):
-            print(f'Processing line no {line_no}') 
-
+    for target_file in args.filenames:
+        http_exception_returns = find_http_exception_returns(target_file)
+        for line_no in http_exception_returns:
+            ret_val = 1
+            print(
+                f'{target_file}:{line_no} returns HTTP exception '
+                '(instead of raising it)',
+                file=sys.stderr,
+            )
     return ret_val
 
 
