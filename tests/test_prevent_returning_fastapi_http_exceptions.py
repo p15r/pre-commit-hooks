3ca38e755408907544b762e9b6d7c3208ef88456
Patrick <patrick@p15r.net>
2025-03-22 21:01:59 +0000

    refactor: switch to `uv` and `tox-uv`

--- tests/test_prevent_returning_fastapi_http_exceptions.py
+++ tests/test_prevent_returning_fastapi_http_exceptions.py
@@ -3,11 +3,11 @@ from pre_commit_hooks.prevent_returning_fastapi_httpexceptions import main
 
 def test_returning_http_exceptions(capsys):
     expected = (
-        'tests/harness/return_httpexception.py:2 returns HTTP exception '
+        'tests/harness/return_httpexception.py:10 returns HTTP exception '
         '(`raise` instead)\n'
-        'tests/harness/return_httpexception.py:8 returns HTTP exception '
+        'tests/harness/return_httpexception.py:17 returns HTTP exception '
         '(`raise` instead)\n'
-        'tests/harness/return_httpexception.py:12 returns HTTP exception '
+        'tests/harness/return_httpexception.py:22 returns HTTP exception '
         '(`raise` instead)\n'
     )
 
